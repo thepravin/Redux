62607a4b7c87968589a5ac4c92c1d2480e03a68b
Pravin Nalawade <thepravin28@gmail.com>
2025-08-31 13:44:43 +0530

    Add HooksCakeContainers component for cake purchasing functionality

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,17 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import CakeContainer from './Components/CakeContainer';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import logo from "./logo.svg";
+import "./App.css";
+import CakeContainer from "./Components/CakeContainer";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import HooksCakeContainers from "./Components/HooksCakeContainers";
 
 function App() {
   return (
     <Provider store={store}>
-    <div className="App">
-      <header className="App-header">
-        <CakeContainer/>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Cake Container</h1>
+          <CakeContainer />
+          <br />
+          <br />
+          <h1>Hooks Cake Container</h1>
+          <HooksCakeContainers />
+        </header>
+      </div>
     </Provider>
   );
 }
---
+++ react-redux/src/Components/HooksCakeContainers.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import { buyCake } from '../redux/cakes/cakeActions'
+
+
+const HooksCakeContainers = () => {
+  const numOfCakes = useSelector((state) => state.numOfCakes)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <h2>Number of Cakes - {numOfCakes}</h2>
+      <button onClick={() => dispatch(buyCake())}>Buy Cake</button>
+    </div>
+  )
+}
+
+export default HooksCakeContainers
