562238bef0373bb6016e7e248ba7e4827e8950da
Pravin Nalawade <thepravin28@gmail.com>
2025-08-31 14:11:08 +0530

    Add  redux devtool package

--- react-redux/src/redux/store.js
+++ react-redux/src/redux/store.js
@@ -1,9 +1,10 @@
-import {  createStore } from "redux";
+import {  applyMiddleware, createStore } from "redux";
 import rootReducer from "./rootReducer";
+import logger from "redux-logger";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 
-
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)))
 
 
 export default store;
